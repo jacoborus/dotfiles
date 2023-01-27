92f4b22a6a9c79c00c19b3e97fddc70d0df0676f
Jacobo Tabernero Rey <jacoborus@gmail.com>
2023-01-27 15:57:19 +0100

    Remove eslint from null-ls because it's already loaded by Mason

--- nvim/plugin_config/null-ls.lua
+++ nvim/plugin_config/null-ls.lua
@@ -3,7 +3,6 @@ local null_ls = require("null-ls")
 null_ls.setup({
   sources = {
     null_ls.builtins.formatting.prettierd,
-    null_ls.builtins.diagnostics.eslint,
     null_ls.builtins.code_actions.gitsigns,
     null_ls.builtins.completion.luasnip
   },
