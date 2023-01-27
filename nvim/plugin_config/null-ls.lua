8682688d2edc26a8838d855de89d3dd1522e9a0f
Jacobo Tabernero Rey <jacoborus@gmail.com>
2023-01-27 21:11:55 +0100

    Add shfmt (sh formatter)

--- nvim/plugin_config/null-ls.lua
+++ nvim/plugin_config/null-ls.lua
@@ -4,6 +4,7 @@ null_ls.setup({
   sources = {
     null_ls.builtins.formatting.prettierd,
     null_ls.builtins.code_actions.gitsigns,
-    null_ls.builtins.completion.luasnip
+    null_ls.builtins.completion.luasnip,
+    null_ls.builtins.formatting.shfmt,
   },
 })
