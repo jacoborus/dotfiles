b55b71abde6ae77053d2d0f04f3cc900149c9556
Jacobo Tabernero Rey <jacoborus@gmail.com>
2023-01-27 14:44:13 +0100

    Remove prettier config, use null-ls

---
+++ nvim/plugin_config/null-ls.lua
@@ -0,0 +1,10 @@
+local null_ls = require("null-ls")
+
+null_ls.setup({
+  sources = {
+    null_ls.builtins.formatting.prettierd,
+    null_ls.builtins.diagnostics.eslint,
+    null_ls.builtins.code_actions.gitsigns,
+    null_ls.builtins.completion.luasnip
+  },
+})
