84d2984ac4ab54296c66778b442fd9377536d8ca
Jacobo Tabernero Rey <jacoborus@gmail.com>
2023-01-27 16:23:39 +0100

    Move cmp config to it own file

--- nvim/plugin_config/lsp.lua
+++ nvim/plugin_config/lsp.lua
@@ -67,7 +67,7 @@ local servers = {
       }
     },
   },
-  tsserver = {},
+  -- tsserver = {},
 }
 
 -- Setup neovim lua configuration
@@ -83,9 +83,11 @@ mason_lspconfig.setup({
   ensure_installed = vim.tbl_keys(servers),
 })
 
+local lspconfig = require('lspconfig')
+
 mason_lspconfig.setup_handlers({
   function(server_name)
-    require('lspconfig')[server_name].setup {
+    lspconfig[server_name].setup {
       capabilities = Capabilities,
       on_attach = on_attach,
       settings = servers[server_name],
@@ -93,62 +95,27 @@ mason_lspconfig.setup_handlers({
   end,
 })
 
-local lspconfig = require('lspconfig')
+-- volar
+lspconfig.volar.setup {
+  on_attach = on_attach,
+  capabilities = Capabilities,
+  filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue', 'json' },
+}
 
+-- denols
 lspconfig.denols.setup {
   on_attach = on_attach,
   capabilities = Capabilities,
   root_dir = lspconfig.util.root_pattern("deno.json", "deno.jsonc"),
 }
 
+-- tsserver
 lspconfig.tsserver.setup {
   on_attach = on_attach,
   capabilities = Capabilities,
+  autostart = false,
   root_dir = lspconfig.util.root_pattern("package.json"),
 }
 
 -- Turn on lsp status information
 require('fidget').setup()
-
--- nvim-cmp setup
-local cmp = require 'cmp'
-local luasnip = require 'luasnip'
-
-cmp.setup {
-  snippet = {
-    expand = function(args)
-      luasnip.lsp_expand(args.body)
-    end,
-  },
-  mapping = cmp.mapping.preset.insert {
-    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete(),
-    ['<CR>'] = cmp.mapping.confirm {
-      behavior = cmp.ConfirmBehavior.Replace,
-      select = true,
-    },
-    ['<Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif luasnip.expand_or_jumpable() then
-        luasnip.expand_or_jump()
-      else
-        fallback()
-      end
-    end, { 'i', 's' }),
-    ['<S-Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
-        luasnip.jump(-1)
-      else
-        fallback()
-      end
-    end, { 'i', 's' }),
-  },
-  sources = {
-    { name = 'nvim_lsp' },
-    { name = 'luasnip' },
-  },
-}
