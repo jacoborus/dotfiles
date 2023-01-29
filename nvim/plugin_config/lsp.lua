420c66e9493f2a6c4dc42c193191059c1fe05bb8
Jacobo Tabernero Rey <jacoborus@gmail.com>
2023-01-29 04:13:20 +0100

    Do not override <c-k>

--- nvim/plugin_config/lsp.lua
+++ nvim/plugin_config/lsp.lua
@@ -27,7 +27,7 @@ local on_attach = function(_, bufnr)
 
   -- See `:help K` for why this keymap
   nmap('<leader>k', vim.lsp.buf.hover, 'Hover Documentation')
-  nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
+  nmap('<leader>K', vim.lsp.buf.signature_help, 'Signature Documentation')
 
   -- Lesser used LSP functionality
   nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
