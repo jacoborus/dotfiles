cac3ce6bc9a7e5c749fc6d9c9a563b561941d397
Jacobo Tabernero <jacoborus@gmail.com>
2024-03-11 23:33:01 +0100

    Higlight when yanking

--- nvim/options.lua
+++ nvim/options.lua
@@ -104,6 +104,15 @@ vim.api.nvim_create_autocmd('BufReadPost', {
   end,
 })
 
+-- Highlight when yanking (copying) text
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
 -- Abbreviations
 vim.cmd [[:iabbrev fucntion function
 :iabbrev fucntino function
