b8ef9587c0f4fe1db61487174037d19a0ea0057c
Jacobo Tabernero <jacoborus@gmail.com>
2024-03-12 02:04:42 +0100

    Enable regexp in LuaSnip

--- nvim/plugins.lua
+++ nvim/plugins.lua
@@ -67,10 +67,20 @@ require('lazy').setup({
 
   { -- Autocompletion
     'hrsh7th/nvim-cmp',
+    event = 'InsertEnter',
     dependencies = {
+      {
+        'L3MON4D3/LuaSnip',
+        build = (function()
+          if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
+            return
+          end
+          return 'make install_jsregexp'
+        end)(),
+      },
+      'saadparwaiz1/cmp_luasnip',
       'hrsh7th/cmp-nvim-lsp',
-      'L3MON4D3/LuaSnip',
-      'saadparwaiz1/cmp_luasnip'
+      'hrsh7th/cmp-path'
     },
   },
 
