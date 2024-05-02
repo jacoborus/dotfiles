ab0f22383577d1b9706f44701ace24b29172946d
Jacobo Tabernero <jacoborus@gmail.com>
2024-05-02 02:45:56 +0200

    Upgrade lua syntax in completions.lua

--- nvim/plugin_config/completions.lua
+++ nvim/plugin_config/completions.lua
@@ -13,7 +13,8 @@ local has_words_before = function()
 	if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then
 		return false
 	end
-	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+	local cursor = { vim.api.nvim_win_get_cursor(0) }
+	local line, col = cursor[1], cursor[2]
 	return col ~= 0 and vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]:match("^%s*$") == nil
 end
 
