000bb58997e5383e67d7bf3f1550d223b4fe9b11
Jacobo Tabernero <jacoborus@gmail.com>
2024-05-07 02:41:29 +0200

    Add dadbod and dadbodui

--- nvim/plugins.lua
+++ nvim/plugins.lua
@@ -110,6 +110,27 @@ require("lazy").setup({
 		-- version = "*"
 	},
 
+	{ -- SQL UI
+		"kristijanhusak/vim-dadbod-ui",
+		dependencies = {
+			{ "tpope/vim-dadbod", lazy = true },
+			{ "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true },
+		},
+		-- cmd = {
+		-- 	"DBUI",
+		-- 	"DBUIToggle",
+		-- 	"DBUIAddConnection",
+		-- 	"DBUIFindBuffer",
+		-- },
+		init = function()
+			-- Your DBUI configuration
+			vim.g.db_ui_use_nerd_fonts = 1
+			vim.cmd([[
+				autocmd FileType sql,mysql,plsql lua require('cmp').setup.buffer({ sources = {{ name = 'vim-dadbod-completion' }} })
+			]])
+		end,
+	},
+
 	{ -- Autoformat
 		"stevearc/conform.nvim",
 		event = { "BufWritePre" },
@@ -126,6 +147,7 @@ require("lazy").setup({
 				markdown = { "deno_fmt" },
 				python = { "isort", "black" },
 				bash = { "shfmt" },
+				vue = { { "prettierd", "prettier" } },
 				yaml = { "yamlfix" },
 				-- json = { { "prettierd", "prettier" } },
 				-- javascript = { { "prettierd", "prettier" } },
